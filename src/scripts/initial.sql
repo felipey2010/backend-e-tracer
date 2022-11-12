1bd051e683f1e9b5968edf219417b17d5f98443b
felipey2010 <mahamaphilip@yahoo.com>
2022-11-12 11:50:17 -0400

    removed script

--- src/scripts/initial.sql
+++
@@ -1,4 +0,0 @@
-
--- PESSOA FÍSICA
-INSERT INTO public.persons(name, cpf, birthdate, email, sex, contact, date_registered)
-    VALUES ('John Doe','70160576237', '1993-06-27', 'devacc2010@gmail.com', 'M', '95991375429', current_timestamp );
